fa02e88742ea97de38c3494bc9142da5657a4a69
Isaiah Moore <imoore@udel.edu>
2026-03-30 12:22:08 -0400

    projects.ts | Created a file where my future projects will go (portfolio content). Added in my CS+ Social Good workshop.

---
+++ src/data/projects.ts
@@ -0,0 +1,11 @@
+import { Project } from '../types/Project';
+
+export const projects: Project[] = [
+    {
+        id: 1.
+        title: "Data Science Pipeline Workshop",
+        description: "Analyzed survival patterns using Python, Pandas, and Matplotlib.",
+        technologies: ["Python", "Pandas", "Matplotlib"],
+        githubLink: "https://github.com/imoore2025/cssg-data-science-pipeline-workshop"
+    }
+];
